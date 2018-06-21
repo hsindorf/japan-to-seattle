1f63d73cefc7e055edf931cad1152979fb154866
Hannah Sindorf <hannah@hsindorf.com>
2018-06-20 23:26:35 -0700

    func to render activities to page

--- js/activities.js
+++ js/activities.js
@@ -1,20 +1,21 @@
-prices = ['< 50', '50-150', '150-500', '500+'];
-types = ['Food', 'Sightseeing', 'Shopping'];
-areas = ['Pike Place Market', 'Around UW Campus', 'Around Space Needle', 'Around Safeco Field', 'Outside of the City'];
+var prices = ['< 50', '50-150', '150-500', '500+'];
+var types = ['Food', 'Sightseeing', 'Shopping'];
+var areas = ['Pike Place Market', 'Around UW Campus', 'Around Space Needle', 'Around Safeco Field', 'Outside of the City'];
 
 Activity.all = [];
 
-function Activity(name, price, type, areas) {
+function Activity(name, price, type, areas, image, maps) {
   this.name = name;
   this.price = price;
   this.type = type;
   this.areas = areas;
-  this.image = '';
-  this.maps = '';
+  this.image = image;
+  this.maps = maps;
   Activity.all.push(this);
 }
 
 function makeLocations() {
+  new Activity('test Location', prices[0], types[0], areas[0], 'https://placehold.it/480x300', 'https://goo.gl/maps/bvptei6TXYx'); 
   new Activity('Pike Place Market', prices[0], types[1], areas[0], 'img/pikeplace.jpg', 'https://goo.gl/maps/bvptei6TXYx');
   new Activity('UW Campus', prices[0], types[1], areas[1]);
   new Activity('Karage Setsuna', prices[0], types[0], areas[2], 'img/karageSetsuna.jpg', 'https://goo.gl/maps/TE3vLmsS6f82');
--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var suitcaseContainer = document.getElementById('suitcase-container');
 var activitiesContainer = document.getElementById('activities-container');
 // stores active filters
 var activeFilters = [false, false, false];
+// stories the filtered activities for this session (array of objects - pulled from local storage on page load, otherwise rechosen when a new filter is selected)
+var activitiesFiltered = [];
 
 // function to run through all filters
 function runFilters() {
@@ -24,10 +26,52 @@ function filter(whichFilter) {}
 // PAGE RENDERING
 
 // renders a single activity
-function renderAnActivity() {}
+function renderAnActivity() {
+  
+}
 
 // renders the list of activities to the page
-function renderTheList() {}
+function renderTheList(renderWhere) {
+  for (var i = 0; i < activitiesFiltered.length; i++) {
+    //outer container
+    var activityContainer = document.createElement('div');
+    activityContainer.setAttribute('class', 'activity-container');
+    renderWhere.appendChild(activityContainer);
+    // image
+    var activityImage = document.createElement('img');
+    activityImage.setAttribute('src', activitiesFiltered[i].image);
+    activityContainer.appendChild(activityImage);
+    // info to right of image
+    var activityInfo = document.createElement('div');
+    activityInfo.setAttribute('class', 'activity-info');
+    activityContainer.appendChild(activityInfo);
+    // to append info - name
+    var activityName = document.createElement('p');
+    activityName.textContent = activitiesFiltered[i].name;
+    activityInfo.appendChild(activityName);
+    // price
+    var activityPrice = document.createElement('p');
+    activityPrice.textContent = activitiesFiltered[i].price;
+    activityInfo.appendChild(activityPrice);
+    // type
+    var activityType = document.createElement('p');
+    activityType.textContent = activitiesFiltered[i].type;
+    activityInfo.appendChild(activityType);
+    // location
+    var activityArea = document.createElement('p');
+    activityArea.textContent = activitiesFiltered[i].area;
+    activityInfo.appendChild(activityArea);
+    // map
+    var activityMapP = document.createElement('p');
+    activityInfo.appendChild(activityMapP);
+    var activityMapA = document.createElement('a');
+    activityMapA.setAttribute('href', activitiesFiltered[i].maps);
+    activityMapA.setAttribute('target', '_blank');
+    activityMapA.textContent = 'Map';
+    activityMapP.appendChild(activityMapA);
+    // add/remove my list
+  }
+}
 
 // LOCAL STORAGE
 
